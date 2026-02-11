ab54b6b70ccee7e72ea1b4c3f6920013ebe2e11c
assislol2024 <assislol2024@gmail.com>
2026-02-11 14:59:16 -0300

    atualizaões

--- script.js
+++ script.js
@@ -1,25 +1,39 @@
-const timer = document.querySelector(".timer");
-let min25 = 60*25
-let tempo = 0
+const domTimer = document.querySelector(".timer");
+
+let tempo = 0;
 
 let setSeconds = (seconds) => {
   let second = new Date(seconds * 1000);
   second = second.toLocaleTimeString("pt-br", {
     timeZone: "UTC",
-    hour12: false,
     minute: "2-digit",
     second: "2-digit",
   });
   return second;
 };
 
+let setMinuts = (min) => {
+  let minuts = 60 * min;
+  return minuts;
+};
 document.addEventListener("click", (e) => {
   let el = e.target.id;
+  tempo = setMinuts(25);
+  console.log(el);
 
   if (el === "start") {
-    timer.innerHTML = setSeconds(min25);
-    close
-  } else if (el == "stop") {
-  } else if (el == "reset") {
+    if (domTimer) {
+      domTimer = setInterval(() => {
+        tempo--;
+        domTimer.innerHTML = setSeconds(tempo);
+      }, 1000);
+    }
+  } else if (el === "stop") {
+    clearInterval(domTimer);
+    domTimer.classList.add("stoped");
+  } else if (el === "reset") {
+    tempo = 0;
+    domTimer.innerHTML = "00:00";
+    clearInterval(domTimer);
   }
 });
