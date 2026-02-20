81923c58b978844e7e8f0e445fb7f4bd0c66416b
assislol2024 <assislol2024@gmail.com>
2026-02-20 09:40:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const domTimer = document.querySelector(".timer");
-let count = 0;
-let state = "stopped";
 
+let timerId = 0;
+let state = "stopped";
 
 let getSeconds = (seconds) => {
   let second = new Date(seconds * 1000);
@@ -18,45 +18,47 @@ let setMinuts = (min) => {
   return minuts;
 };
 let tempo = setMinuts(25);
+
+
 document.addEventListener("click", (e) => {
   let el = e.target.id;
 
-
-  
-  function setState(e){
-    state = e
+  function setState(e) {
+    state = e;
   }
+
   switch (el) {
     case "start":
       if (state === "stopped") {
-        count = setInterval(() => {
+        timerId = setInterval(() => {
           tempo--;
-          domTimer.innerHTML = getSeconds(tempo);
           if (tempo === 0) {
             domTimer.classList.add("stoped");
             alert("Acabou o tempo!!!");
-            clearInterval(count);
+            clearInterval(timerId);
           }
+          domTimer.textContent = getSeconds(tempo);
         }, 1000);
+
         domTimer.classList.remove("stoped");
-        setState("running")
+        setState("running");
       }
       break;
     case "stop":
       if (state === "running") {
-        clearInterval(count);
+        clearInterval(timerId);
         domTimer.classList.add("stoped");
-        setState("stopped")
+        setState("stopped");
       }
       console.log(state);
       break;
     case "reset":
-      if (state) {
-        clearInterval(count);
+      if (state === "running" || state === "stopped") {
+        clearInterval(timerId);
         domTimer.classList.remove("stoped");
         tempo = setMinuts(25);
         domTimer.innerHTML = "25:00";
-        setState("stopped")
+        setState("stopped");
       }
       console.log(state);
       break;
